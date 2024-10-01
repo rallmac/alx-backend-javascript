e3a1c1f8469cec8c092749bddc2e0857f5eb489c
rallmac2 <root@tobijahdevice>
2024-10-01 20:08:24 +0100

    task 0 redo

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,17 @@
-#!/usr/bin/node
-//Javascript code
+// task.js
 
 export function taskFirst() {
-	const task = 'I prefer const when I can.';
-	return task;
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
 export function getLast() {
-	return ' is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
-	let combination = 'Bit sometimes let';
-	combination += getLast();
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-	return combination;
+  return combination;
 }
