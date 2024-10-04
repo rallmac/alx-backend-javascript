f3e2314cdd7ba99c6e6bbb97ac2a286f50d11e31
rallmac2 <root@tobijahdevice>
2024-10-04 11:12:43 +0100

    task 2 again4

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,4 @@
-export default function handleResponseFromAPI (promise) {
+function handleResponseFromAPI (promise) {
   return promise
     .then(() => {
       return {
@@ -13,3 +13,5 @@ export default function handleResponseFromAPI (promise) {
       console.log('Got a response from the API');
     });
 }
+
+export default handleResponseFromAPI;
