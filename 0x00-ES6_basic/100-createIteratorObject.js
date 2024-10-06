2a551e51880241b1a1e1d512ead6c926d55d1669
rallmac2 <root@tobijahdevice>
2024-10-06 22:00:37 +0100

    task 100 redo

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,9 +2,10 @@ export default function createIteratorObject(report) {
   const employees = [];
 
   for (const department in report.allEmployees) {
-    employees.push(...report.allEmployees[department]);
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+      employees.push(...report.allEmployees[department]);
+    }
   }
 
   return employees[Symbol.iterator]();
 }
-
