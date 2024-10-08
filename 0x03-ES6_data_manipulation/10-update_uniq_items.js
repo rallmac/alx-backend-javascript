21bbac2d0040b707dd5742735d0963d78cc36c52
rallmac2 <root@tobijahdevice>
2024-10-08 20:21:45 +0100

    task 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItem(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+
+  return map;
+}
