2ef55404de3328475f6d3531b57cfae3fff670d6
rallmac <tobijahekperikpe@gmail.com>
2024-10-12 08:03:46 +0100

    task 2 again

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -10,6 +10,9 @@ class HolbertonCourse {
   }
 
   set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = value;
   }
 
@@ -18,6 +21,9 @@ class HolbertonCourse {
   }
 
   set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = value;
   }
 
@@ -26,6 +32,9 @@ class HolbertonCourse {
   }
 
   set students(value) {
+    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._students = value;
   }
 }
