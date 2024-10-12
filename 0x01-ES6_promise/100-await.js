917f0da83624adae92ab02edc8c2cc7d96afba5c
rallmac <tobijahekperikpe@gmail.com>
2024-10-12 18:51:26 +0100

    task 100 redo

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,8 +1,17 @@
-import asyncUploadUser from './asyncUploadUser';
+import { uploadPhoto, createUser } from './utils';
 
-async function test() {
-  const value = await someAsyncFunction();
-  console.log(value);
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }
-
-test();
