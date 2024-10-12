c018ee07aeb028fba7a621de9b48b4589d614601
rallmac <tobijahekperikpe@gmail.com>
2024-10-12 09:04:24 +0100

    task 7

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,13 @@
-class Building {
+export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+
     if (new.target === Building) {
       throw new Error('Building is an abstract class and cannot be instantiated directly');
+    }
+
+    if (this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
@@ -11,10 +16,6 @@ class Building {
   }
 
   evacuationWarningMessage() {
-    // Using `this` to comply with the `class-methods-use-this` rule
-    console.log(this._sqft);
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+    throw new Error('evacuationWarningMessage must be implemented');
   }
 }
-
-export default Building;
