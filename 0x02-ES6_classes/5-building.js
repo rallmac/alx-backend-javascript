91178b7a1cfde65ed1ee80e39f3149ecea64795d
rallmac <tobijahekperikpe@gmail.com>
2024-10-12 08:44:44 +0100

    task 5 redo

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,55 +1,8 @@
-/** export default class Building {
+class Building {
   constructor(sqft) {
     this._sqft = sqft;
-  }
-
-  get sqft() {
-    return this._sqft;
-  }
-
-  set sqft(sqft) {
-    if (sqft === undefined || sqft === null) {
-      // Check if sqft is undefined or null.
-      throw new Error('sqft must be defined');
-    } else if (typeof sqft === 'string') {
-      // Check if sqft is a string.
-      throw new Error('sqft must be a number');
-    } else if (typeof sqft !== 'number' || sqft <= 0) {
-      // Check if sqft is not a positive number.
-      throw new Error('sqft must be a positive number');
-    } else if (!Number.isFinite(sqft)) {
-      // Check if sqft is not a finite number.
-      throw new Error('sqft must be a finite number');
-    }
-    this._sqft = Number(sqft);
-  }
-  }
-
-  if (require.main === module) {
-    const b = new Building(100);
-    console.log(b);
-
-    class TestBuilding extends Building {}
-
-    try {
-      new TestBuilding(200);
-    }
-    catch(err) {
-    console.log(err);
-    }
-  }
-* */
-export default class Building {
-  constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = sqft;
-
-    if (new.target !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Bulding must override evacuationWarningMessage');
-      }
+    if (new.target === Building) {
+      throw new Error('Building is an abstract class and cannot be instantiated directly');
     }
   }
 
@@ -57,7 +10,11 @@ export default class Building {
     return this._sqft;
   }
 
-  static evacuationWarningMessage() {
+  evacuationWarningMessage() {
+    // Using `this` to comply with the `class-methods-use-this` rule
+    console.log(this._sqft);
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
+
+export default Building;
