21c7a42d14e378b141e02237d2b7be3594b4c6f2
rallmac <tobijahekperikpe@gmail.com>
2024-10-12 09:47:46 +0100

    task 6 again

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -13,6 +13,6 @@ export default class SkyHighBuilding extends Building {
 
   // Override evacuationWarningMessage
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
