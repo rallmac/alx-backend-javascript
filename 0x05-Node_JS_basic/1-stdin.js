b6c0c861331a3f3c8ea6b9122a3d3f455f44440f
rallmac <tobijahekperikpe@gmail.com>
2024-11-26 10:32:50 +0100

    Task 1 correction

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,11 @@
-if (process.stdin.isTTY) {
-  process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-  process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit();
-  });
-} else {
-  let input = '';
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.exit();
+});
 
-  process.stdin.on('data', (data) => {
-    input += data.toString();
-  });
-
-  process.stdin.on('end', () => {
-    const name = input.trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    console.log('This important software is now closing');
-  });
-}
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
